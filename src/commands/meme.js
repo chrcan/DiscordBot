2a0c85559c84a72294a80706a25732b2f82b57ce
Christian <werbegrafik@rc-design.at>
2023-01-18 17:40:42 +0100

    new meme api.giphy.com

--- src/commands/meme.js
+++ src/commands/meme.js
@@ -1,6 +1,7 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
-const { EmbedBuilder } = require("discord.js")
-const fetch = require('node-fetch')
+//const { SlashCommandBuilder } = require("@discordjs/builders")
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+//const fetch = require('node-fetch')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 // const fetch = require('node-fetch')
 
@@ -9,28 +10,48 @@ module.exports = {
     .setName("meme")
     .setDescription("Holen dir ein Meme!"),
   async execute(interaction) {
+    /*
+        async function meme() {
+          await fetch('https://api.giphy.com/v1/gifs/random?api_key=CHfe77jLAvosBc1QAtX2Sa4EIMVcKwPP&tag=Bodybuilding&rating=g')
+            .then(async res => {
+              let meme = await res.json();
+    
+              //console.log(meme);
+    
+              let title = meme[0].data.children[0].data.title;
+              let image = meme[0].data.children[0].data.url;
+              let author = meme[0].data.children[0].data.author;
+    
+              const embed = new EmbedBuilder()
+                .setColor("Yellow")
+                .setTitle(`${title}`)
+                .setImage(`${image}`)
+                .setURL(`${image}`)
+                .setFooter({ text: author })
+    
+              await interaction.reply({ embeds: [embed] });
+            });
+        }
+        */
+    const { guild, options, member } = interaction;
 
-    async function meme() {
-      await fetch('https://www.reddit.com/r/memes/random/.json')
-        .then(async res => {
-          let meme = await res.json();
+    const embed = new EmbedBuilder();
 
-          //console.log(meme);
+    async function giphyMeme() {
+      await fetch('https://api.giphy.com/v1/gifs/random?api_key=CHfe77jLAvosBc1QAtX2Sa4EIMVcKwPP&tag=Bodybuilding&rating=g').then(async res => {
+        let meme = await res.json();
 
-          let title = meme[0].data.children[0].data.title;
-          let image = meme[0].data.children[0].data.url;
-          let author = meme[0].data.children[0].data.author;
+        let title = meme.data.title;
+        let url = meme.data.images.original.url;
+        let link = meme.data.url;
+        let author = meme.data.user.display_name;
+        let pf = meme.data.user.avatar_url;
 
-          const embed = new EmbedBuilder()
-            .setColor("Yellow")
-            .setTitle(`${title}`)
-            .setImage(`${image}`)
-            .setURL(`${image}`)
-            .setFooter({ text: author })
-
-          await interaction.reply({ embeds: [embed] });
+        await interaction.reply({
+          embeds: [embed.setTitle(title).setImage(url).setURL(url).setColor("Random").setFooter({ text: author, iconURL: pf })],
         });
+      });
     }
-    meme();
+    giphyMeme();
   }
 }
